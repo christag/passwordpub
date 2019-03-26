0951740d84f4043f5583929f5418199c7894e403
christag <chris.tagliaferro@gmail.com>
2019-03-26 12:10:57 -0400

    Comments

--- start_server.py
+++ start_server.py
@@ -4,12 +4,12 @@
 
 # Import Flask
 from flask import Flask,render_template,request
-import os 
 
 # Import other parts of PasswordPub
 from bar_kit.shaker import mix_drink
 from bar_kit.ops_manual import open_bar
 
+# Define class object for a password order, expects Bar class object as an input.
 class Drink_order(object):
   def __init__(self, config):
     self.min_char = config.min_char
@@ -22,10 +22,10 @@ class Drink_order(object):
 
 # Create a password using the supplied parameters
 def fulfill_order(order,bar):
-  drink_order = Drink_order(bar.default_options)
+  drink_order = Drink_order(bar.default_options) # Create a new Drink_order object for this order, set it initially to the bar's default options.
   for parameter in order: # Checks paremeters in query.
-    if hasattr(drink_order,parameter) == True:
-      setattr(drink_order, parameter, order.get(parameter))
+    if hasattr(drink_order,parameter) == True: # Is parameter an actual parameter?
+      setattr(drink_order, parameter, order.get(parameter)) # If so, update the Drink_order object with new options.
     else:
       return 'Error: Bad parameter in request!' # If there's a bad parameter, fail.
   cocktail = mix_drink(drink_order,bar) # Create password using the configuration.
@@ -38,13 +38,13 @@ def create_app(bar):
   @app.route("/") # Endpoint for HTML in Browser
   def display_password():
     #pylint: disable=unused-variable
-    cocktail = fulfill_order(request.args,bar)
+    cocktail = fulfill_order(request.args,bar) # Create a password using the parameters in the URL plus the bar's default options.
     return render_template('index.html', password=cocktail) # Returns a rendered version of index.html with the password displayed.
   
   @app.route('/api') # Endpoint for API calls.
   def generate_password():
     #pylint: disable=unused-variable
-    cocktail = fulfill_order(request.args,bar)
+    cocktail = fulfill_order(request.args,bar) # Create a password using the parameters in the URL plus the bar's default options.
     return cocktail # Returns a plaintext password.
 
   return app
